228ca81d5e4b01e40126380f2ca15ea854c50137
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-29 23:07:57 +0000

    version 4.3.9
    git-svn-id: https://plugins.svn.wordpress.org/facebook-comments-master/tags/4.3.9@905134 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/facebook-comments-master-admin-widgets-table.php
@@ -0,0 +1,48 @@
+<?php
+if(!class_exists('WP_List_Table')){
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+class facebook_comments_master_admin_widgets_table extends WP_List_Table {
+	/**
+	 * Display the rows of records in the table
+	 * @return string, echo the markup of the rows
+	 */
+	function display() {
+?>
+<table class="widefat fixed" cellspacing="0">
+	<thead>
+		<tr>
+			<th id="columnname" class="manage-column column-columnname" scope="col" width="300"><legend><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Screenshot', 'facebook_comments_master'); ?></h3></legend></th>
+			<th id="columnname" class="manage-column column-columnname" scope="col"><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Description', 'facebook_comments_master'); ?></h3></legend></th>
+		</tr>
+	</thead>
+
+	<tfoot>
+		<tr>
+			<th class="manage-column column-columnname" scope="col" width="300"><a class="button-primary" href="/wp-admin/widgets.php" title="To Widgets Page" style="float:left;">To Widgets Page</a></p></th>
+			<th class="manage-column column-columnname" scope="col"><a class="button-primary" href="/wp-admin/widgets.php" title="To Widgets Page" style="float:right;">To Widgets Page</a></p></th>
+		</tr>
+	</tfoot>
+
+	<tbody>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-facebookcommentsmaster-admin-widget-viral.png', __FILE__); ?>" alt="<?php echo get_option('facebook_comments_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Facebook Comments Master Viral Widget</h3><p>The Viral Widget is packed with html5 Facebook Like and Share button to turn your wordpress website "virulent". Watch those visits and new users Boom! Works much like the Twitter Tweet by sharing your website pages.</p><p>Looks great when published under any of the below widgets, remember you can always hide the widget title to get the button closer to the below widgets.</p><p>Navigate to your wordpress widgets page and start using it.</p></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-facebookcommentsmaster-admin-widget-basic.png', __FILE__); ?>" alt="<?php echo get_option('facebook_comments_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Facebook Comments Master Basic Fast Loading Widget</h3><p>This Widget was specially designed in html5 to be easy to use and deploy, all settings are on <b>auto mode</b>. Extremely fast page load times and small system trace.</p><p><b>Fully Mobile Responsive</b>.</p><p>Navigate to your wordpress widgets page and start using it.</p></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-facebookcommentsmaster-admin-widget-advanced.png', __FILE__); ?>" alt="<?php echo get_option('facebook_comments_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Facebook Comments Master Advanced Responsive Widget</h3><p>This is the "top of the line" Facebook Comments Widget specially designed for heavy duty professional wordpress websites where fast page load times, stability and perfect Google SEO is a must.</p><p>Coded in html5 it's packed with display options and is <b>Fully Mobile Responsive</b>.</p><p>Check Add-ons Page.</p></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-admin-widget-blank.png', __FILE__); ?>" alt="<?php echo get_option('amazon_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Suggest a Widget</h3><p>Would you like to see your widget idea added to this plugin? Just drop us a line and we will make sure it gets included in the next release.</p><p>Get in touch with TechGasp.</p></td>
+		</tr>
+	</tbody>
+</table>
+<?php
+		}
+}
